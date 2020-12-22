1c2e72f914bd651ef3a4056f0245dd32ef5c5511
Amirtharathna <amirtha800@gmail.com>
2020-12-22 15:51:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-2vhp79
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-2vhp79)
---
+++ index.ts
@@ -0,0 +1,8 @@
+let message = "hello world";
+console.log(message);
+// Import stylesheets
+import "./style.css";
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
